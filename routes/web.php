bca92d8485b37ad1d21f8436ae33890d82241d56
Angry Red <angryred027@gmail.com>
2022-12-06 19:42:50 +0900

    Chapter2: 複数アクションの利用

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{id?}/{pass?}', 'HelloController@index');
-// Route::get('hello/other', 'HelloController@other');
+Route::get('hello/', 'HelloController@index');
+Route::get('hello/other', 'HelloController@other');
