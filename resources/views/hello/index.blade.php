9627ca2d6f6dc111c96d31d9bbb44cd0773b79d4
Angry Red <angryred027@gmail.com>
2022-12-28 20:17:11 +0900

    Chapter3: @breakと@continue

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -24,14 +24,18 @@
         Blade/Index
     </h1>
     <p>
-        &#064;foreachディレクティブの例
+        &#064;forディレクティブの例
     </p>
     <ol>
-        @foreach($data as $item)
-            <li>
-                {{ $item }}
-                @endforeach
-            </li>
+        @for ($i = 1; $i < 100; $i++ )
+            @if ($i % 2 == 1)
+                @continue
+            @elseif ($i <= 10)
+                <li>No, {{ $i }}
+            @else
+                @break
+            @endif
+        @endfor
     </ol>
 
     <form action="/hello" method="POST">
